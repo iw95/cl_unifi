f9c7eba896aa889ace6215673bad466e73824647
iw95 <leorumi@mnet-online.de>
2023-02-07 21:46:57 +0100

    initial data setup more accessible

---
+++ setup_data.py
@@ -0,0 +1,8 @@
+import filter
+import preprocess
+
+
+# Filter data to use for training based on class sizes
+# creates speech-accent-archive/speakers_use.csv and speech-accent-archive/overview.csv
+filter()
+preprocess()
